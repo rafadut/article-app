a51ed22abc1ccac2f1e88d25e8b8d74ce431ff98
Rafael Dutra <rafaeldut89@gmail.com>
2018-01-16 20:55:45 -0200

    1.0

--- ArticleApp/Controllers/HomeController.cs
+++ ArticleApp/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ArticleApp.Models;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -26,5 +28,43 @@ namespace ArticleApp.Controllers
 
             return View();
         }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(Article article)
+        {
+            if (ModelState.IsValid)
+            {
+                //db.Articles.Add(article);
+                //db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(article);
+        }
+
+        //public JsonResult IsUserAvailable(string username)
+        //{
+        //    if (!WebSecurity.UserExists(username))
+        //    {
+        //        return Json(true, JsonRequestBehavior.AllowGet);
+        //    }
+        //    string suggestedUID = String.Format(CultureInfo.InvariantCulture,
+        //    "{0} is not available.", username);
+        //    for (int i = 1; i < 100; i++)
+        //    {
+        //        string altCandidate = username + i.ToString();
+        //        if (!WebSecurity.UserExists(altCandidate))
+        //        {
+        //            suggestedUID = String.Format(CultureInfo.InvariantCulture,
+        //            "{0} is not available. Try {1}.", username, altCandidate);
+        //            break;
+        //        }
+        //    }
+        //    return Json(suggestedUID, JsonRequestBehavior.AllowGet);
+        //}
     }
 }
---
+++ ArticleApp/Models/Article.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ArticleApp.Models
+{
+    public class Article
+    {
+        public int ID { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 5)]
+        public string Title { get; set; }
+        [RegularExpression(@"[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}")]
+        public string AuthorEmail { get; set; }
+        [DataType(DataType.Date)]
+        [Range(300, 3000)]
+        public int NumberOfAuthors { get; set; }
+        [Required]
+        public DateTime CreateDate { get; set; }
+        [Required]
+        public string Description { get; set; }
+        [Range(1, 250)]
+        [DataType(DataType.Currency)]
+        [Required]
+        public decimal Price { get; set; }
+        [Required]
+        [StringLength(6, MinimumLength = 3)]
+        [Remote("IsUserAvailable", "Validation")]
+        [RegularExpression(@"(\S)+", ErrorMessage = "White space is not allowed.")]
+        [Editable(true)]
+        public string UserName { get; set; }
+    }
+
+    //public class ArticleDBContext : DbContext
+    //{
+    //    public DbSet<Article> Articles { get; set; }
+    //}
+}
