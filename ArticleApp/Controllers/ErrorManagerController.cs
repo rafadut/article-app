5f91c801ca97a8946e983230ae21e5daf32bedac
Rafael Dutra <rafaeldut89@gmail.com>
2018-01-31 17:45:31 -0200

    handling first chance exceptions

---
+++ ArticleApp/Controllers/ErrorManagerController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ArticleApp
+{
+    internal class ErrorManagerController : IController
+    {
+        public void Execute(RequestContext requestContext)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- ArticleApp/Global.asax.cs
+++ ArticleApp/Global.asax.cs
@@ -12,10 +12,40 @@ namespace ArticleApp
     {
         protected void Application_Start()
         {
+            AppDomain.CurrentDomain.FirstChanceException += CurrentDomain_FirstChanceException;
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        private void CurrentDomain_FirstChanceException(object sender, System.Runtime.ExceptionServices.FirstChanceExceptionEventArgs e)
+        {
+            if (e.Exception is NotImplementedException)
+            {
+                // do something special when the functionality is not implemented
+            }
+        }
+
+        public void Application_Error(Object sender, EventArgs e)
+        {
+            if (Server != null)
+            {
+                //Get the context
+                HttpContext appContext = ((MvcApplication)sender).Context;
+                Exception ex = Server.GetLastError().GetBaseException();
+                //Log the error using the logging framework
+                //Logger.Error(ex);
+                //Clear the last error on the server so that custom errors are not fired
+                Server.ClearError();
+                //forward the user to the error manager controller.
+                IController errorController = new ErrorManagerController();
+                RouteData routeData = new RouteData();
+                routeData.Values["controller"] = "ErrorManagerController";
+                routeData.Values["action"] = "ServerError";
+                errorController.Execute(
+                new RequestContext(new HttpContextWrapper(appContext), routeData));
+            }
         }
     }
 }
